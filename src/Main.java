f25b9774bdcf647f88fb2494a3fd2b9fde2eb08e
VitalyDabryan <vital.1977@tut.by>
2022-07-02 23:03:58 +0300

    3d commit

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
             int start = matcher.start();
             int end = matcher.end();
             System.out.println(text.substring(start, end));
-            System.out.println(text.substring(start, end));
+
         }
 
     }

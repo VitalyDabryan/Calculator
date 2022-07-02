8251530a7201314663e7c3f2f044641886ca8b72
VitalyDabryan <vital.1977@tut.by>
2022-07-02 22:19:19 +0300

    4 commit

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,26 @@
-import java.util.Scanner;
-class Main {
-    public static void main(String args[]) throws java.io.IOException{
-        System.out.print("Number1: ");
-        Scanner scan = new Scanner(System.in);
-        int a, b, result = 0;
-        char c = '+';
-        a = scan.nextInt();
-        while (c != 's') {
-            System.out.print("Enter '+,-,*,/,'c' or 's': ");
-            c = (char) System.in.read ();
-            if (c == '-' || c =='+' || c == '*' || c == '/') {
-                System.out.print("Number 2: ");
-                b = scan.nextInt();
-                switch (c) {
-                    case '+':
-                        result = a + b;
-                        break;
-                    case  '-':
-                        result = a - b;
-                        break;
-                    case  '*':
-                        result = a * b;
-                        break;
-                    case  '/':
-                        result = a / b;
-                        break;
-                };
-                System.out.println ("Result: " + result);
-            }
-            else {
-                if (c == 'c') {
-                    result = 0;
-                    System.out.println ("Result: " + result);
-                    c = (char) System.in.read();
-                } else
-                if (c != 's') {
-                    System.out.println("Nekorrectny simbol!");
-                    c = (char) System.in.read();
-                    result = a;}
-                else break;
-            }
-            a = result;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Main {
+    public static void main(String[] args) {
+        String text = "SW Eltex MES1124 УД АТС-66 388-22-66-СОШ №1 MES-1124 (172.16.16.93):03 /nSW D-Link DES-1210-28/ME УД АТС-45 МБОУ DES-1210-28 (172.16.252.170) /nSW D-Link DES-1210-28/ME 385-4 Бийск ПСЭ-32/2 32/2-106/5(ОКН) DES-1210-28(ОКН) (10.50.140.206):02 /nкрс 2036 35 (1) штк СТШ2036-001 d2036-3(10.54.20.124) 0/0";
+        String newText = "";
+        String stringTest = "([0-9]{1,3}[\\.]){3}[0-9]{1,3}";
+        //    Pattern pattern = Pattern.compile("([0-9]{1,3}[\\.]){3}[0-9]{1,3}");
+        Pattern pattern = Pattern.compile(stringTest);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+            System.out.println(text.substring(start, end));
         }
+
     }
+
 }
+
+// public class Main
+// {
+//     public static void main(String[] args) {
+//         System.out.println("Welcome to Online IDE!! Happy Coding :)");
+//     }
+// }
